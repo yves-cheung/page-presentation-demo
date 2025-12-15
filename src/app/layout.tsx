934bb085668a73184ac0d4190d978b031c4269a7
Yves Cheung <yves.c.dev@gmail.com>
2025-12-15 14:43:30 +0800

    update meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0" />
       </head>
       <body
         className={`${inter.variable} antialiased`}
